ae1cec7540f01fc7ffff55d6f92696896feab84f
thepsycongroo <znaifer9@gmail.com>
2026-06-22 14:52:00 -0600

    enums
    enums creados

---
+++ CommonHotel/src/main/java/com/aa/common/enums/EstadoRegistro.java
@@ -0,0 +1,11 @@
+package com.aa.common.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum EstadoRegistro {
+    ACTIVO,
+    ELIMINADO;
+}
