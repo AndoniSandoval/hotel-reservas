072a9ade38f24b4af6faa5b0bfce01ae27ea3a4b
AndoniSandoval <andoni2020lopez@gmail.com>
2026-06-25 12:45:29 -0600

    feat: creacion de dtos Response, Request y UpdateRequest

--- ms-reservas/src/main/java/com/aa/reservas/dto/ReservaResponse.java
+++ ms-reservas/src/main/java/com/aa/reservas/dto/ReservaResponse.java
@@ -1,5 +1,19 @@
 package com.aa.reservas.dto;
 
-public record ReservaResponse() {
+import java.time.LocalDateTime;
 
-}
+import com.aa.common.enums.EstadoRegistro;
+import com.aa.common.enums.EstadoReserva;
+
+public record ReservaResponse(
+		Long idReserva,
+	    Long idHuesped,
+	    Long idHabitacion,
+	    LocalDateTime fechaEntrada,
+	    LocalDateTime fechaSalida,
+	    EstadoReserva estadoReserva,
+	    EstadoRegistro estadoRegistro,
+	    LocalDateTime fechaCreacion,
+	    LocalDateTime fechaActualizacion
+	 
+		) {}
